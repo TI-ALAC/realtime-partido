a7ec5c5a00348c6307222382b30fadf2758ed27d
Luis Toledo <luis@hotmail.com>
2024-05-08 14:01:17 -0500

    scmbios en img

--- pozo.js
+++ pozo.js
@@ -1,4 +1,4 @@
-const URI = "https://widgets.fn.sportradar.com/betinaction2/es/Etc:UTC/gismo/match_timelinedelta/48613115?T=exp=1715194229~acl=/*~data=eyJvIjoiaHR0cHM6Ly9lY3VhYmV0LmNvbSIsImEiOiJlZTExYzI1NmQxYTliYmM1MzBiNGEzMzgwZGFlODY5OCIsImFjdCI6Im9yaWdpbmNoZWNrIiwib3NyYyI6Im9yaWdpbiJ9~hmac=cfb756eeeaf731e64124fd426dc4e174a43bec092aa25c2f1a00ba3ba14d6aee";
+const URI = "https://widgets.fn.sportradar.com/betinaction2/es/Etc:UTC/gismo/match_timelinedelta/48613119?T=exp=1715280629~acl=/*~data=eyJvIjoiaHR0cHM6Ly9lY3VhYmV0LmNvbSIsImEiOiJlZTExYzI1NmQxYTliYmM1MzBiNGEzMzgwZGFlODY5OCIsImFjdCI6Im9yaWdpbmNoZWNrIiwib3NyYyI6Im9yaWdpbiJ9~hmac=d664038ebca8edf3a6bfd1108793e30d4aaf8538628af97c0683f972129d3355";
 // const URI2 = "https://sb2frontend-altenar2.biahosted.com/api/widget/GetEventDetails?culture=es-ES&timezoneOffset=300&integration=ecuabet&deviceType=1&numFormat=en-GB&countryCode=PE&eventId=9582294";
 
 var content = document.getElementById('contenido');
